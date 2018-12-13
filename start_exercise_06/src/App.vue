b4fac43b81dc44740352ca918f43a18daf9c2f21
Luis C. Berrocal <lberrocal@pancanal.com>
2018-12-13 12:35:39 -0500

    Finished assignment

--- start_exercise_06/src/App.vue
+++ start_exercise_06/src/App.vue
@@ -3,15 +3,7 @@
         <server-header></server-header>
         <hr>
         <div class="row">
-            <div class="col-xs-12 col-sm-6">
-                <ul class="list-group">
-                    <li
-                            class="list-group-item"
-                            v-for="index in 5">
-                        Server #{{ index }}
-                    </li>
-                </ul>
-            </div>
+           <servers></servers>
            <server-details></server-details>
         </div>
         <hr>
--- start_exercise_06/src/main.js
+++ start_exercise_06/src/main.js
@@ -3,10 +3,12 @@ import App from './App.vue'
 import ServerDetails from './ServerDetails.vue'
 import ServerFooter from './ServerFooter.vue'
 import ServerHeader from './ServerHeader.vue'
+import Servers from './Servers.vue'
 
 Vue.component('server-header', ServerHeader)
 Vue.component('server-footer', ServerFooter)
 Vue.component('server-details', ServerDetails)
+Vue.component('servers', Servers)
 
 new Vue({
   el: '#app',
