f6f5d41e9ac5c4ace3363b0e30626a705a82986a
Luis C. Berrocal <lberrocal@pancanal.com>
2018-12-13 12:29:14 -0500

    Added components for footer, header and details

--- start_exercise_06/src/App.vue
+++ start_exercise_06/src/App.vue
@@ -1,12 +1,6 @@
 <template>
     <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <header>
-                    <h1>Server Status</h1>
-                </header>
-            </div>
-        </div>
+        <server-header></server-header>
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-6">
@@ -18,18 +12,10 @@
                     </li>
                 </ul>
             </div>
-            <div class="col-xs-12 col-sm-6">
-                <p>Server Details are currently not updated</p>
-            </div>
+           <server-details></server-details>
         </div>
         <hr>
-        <div class="row">
-            <div class="col-xs-12">
-                <footer>
-                    <p>All Servers are managed here</p>
-                </footer>
-            </div>
-        </div>
+        <server-footer></server-footer>
     </div>
 </template>
 
--- start_exercise_06/src/main.js
+++ start_exercise_06/src/main.js
@@ -1,5 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ServerDetails from './ServerDetails.vue'
+import ServerFooter from './ServerFooter.vue'
+import ServerHeader from './ServerHeader.vue'
+
+Vue.component('server-header', ServerHeader)
+Vue.component('server-footer', ServerFooter)
+Vue.component('server-details', ServerDetails)
 
 new Vue({
   el: '#app',
