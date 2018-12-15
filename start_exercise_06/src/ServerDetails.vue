3261bacb30b05663c41b90373d1f2a8127bad86a
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2018-12-15 09:26:02 -0500

    Added change status buttons

--- start_exercise_06/src/ServerDetails.vue
+++ start_exercise_06/src/ServerDetails.vue
@@ -2,6 +2,8 @@
     <div class="col-xs-12 col-sm-6">
         <p v-if="!server">Please select a server</p>
         <p v-else>Server {{ server.id }} selected Status {{server.status}}</p>
+        <button @click="changeStatus('Normal')" v-if="server.status != 'Normal'">Normal</button>
+        <button @click="changeStatus('Critical')" v-if="server.status != 'Critical'">Critical</button>
     </div>
 </template>
 
@@ -14,6 +16,11 @@
             return {
                 server: null
             }
+        },
+        methods: {
+          changeStatus(status){
+              this.server.status = status;
+          }
         },
         created() {
             eventBus.$on('serverSelected', (server) => {
