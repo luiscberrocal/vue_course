5f49f7b61985c8ecf07f63a2e632a6fa9307d62e
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2018-12-15 11:19:07 -0500

    Finished excerise 7

---
+++ exercise_07/src/components/Green.vue
@@ -0,0 +1,28 @@
+<template>
+     <div>
+        <slot name="title">Title</slot>
+        <div class="row content">
+           <slot name="content">Content</slot>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    div {
+        border: 1px solid green;
+        background-color: lightgreen;
+        padding: 30px;
+        margin: 20px auto;
+        text-align: center
+    }
+    .content {
+         border: 0px;
+        padding: 5px;
+        margin: 5px auto;
+        text-align: left;
+    }
+</style>
