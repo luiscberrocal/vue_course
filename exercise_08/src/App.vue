9a9504aa0c77d6249774c59efdda68766181439e
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2018-12-15 12:21:52 -0500

    Added progress bar

---
+++ exercise_08/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+
+    <div class="container">
+        <h1>Quotes</h1>
+        <progress-bar></progress-bar>
+    </div>
+</template>
+
+<script>
+    import ProgressBar from './components/ProgressBar.vue'
+
+    export default {
+        components: {
+            progressBar: ProgressBar
+        }
+
+    }
+</script>
+
+<style>
+</style>
---
+++ exercise_08/src/components/ProgressBar.vue
@@ -0,0 +1,28 @@
+<template>
+    <section class="row">
+        <div class="small-6 columns">
+            <div class="progress-bar">
+                <div class="progress-bar text-center" style="background-color: green; margin: 0; color: white;"
+                     :style="{width: '100%'}" >
+                    1/10
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: "ProgressBar"
+    }
+</script>
+
+<style scoped>
+    .progress-bar {
+        width: 80%;
+        height: 40px;
+        background-color: #eee;
+        margin: auto;
+        transition: width 500ms;
+    }
+</style>
