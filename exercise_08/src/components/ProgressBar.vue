bfe586dae05a5318216c4a4159a84ec3e8572ea2
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2018-12-16 10:27:36 -0500

    Changed progress bar

--- exercise_08/src/components/ProgressBar.vue
+++ exercise_08/src/components/ProgressBar.vue
@@ -1,36 +1,38 @@
 <template>
-    <section class="row">
-        <div class="small-6 columns">
-            <div class="progress-bar">
-                <div class="progress-bar text-center" style="background-color: green; margin: 0; color: white;"
-                     :style="{width: quoteCount/10 * 100 + '%'}" >
-                    {{quoteCount}}/10
+    <div class="row">
+        <div class="col-md-12">
+            <h3>Quotes</h3>
+            <div class="progress">
+                <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"
+                     :style="{width: (quoteCount/10)*100 + '%'}">
+                    {{ quoteCount}} / 10
                 </div>
             </div>
         </div>
-    </section>
+    </div>
 </template>
 
 <script>
     import {eventBus} from "../main";
+
     export default {
-        props:{
-          quoteCount: {
-              type: Number,
-              required: true,
-              default: 1
-          }
+        props: {
+            quoteCount: {
+                type: Number,
+                required: true,
+                default: 1
+            }
         },
         name: "ProgressBar"
     }
 </script>
 
 <style scoped>
-    .progress-bar {
-        width: 80%;
-        height: 40px;
-        background-color: #eee;
-        margin: auto;
-        transition: width 500ms;
-    }
+    /* .progress-bar {
+         width: 80%;
+         height: 40px;
+         background-color: #eee;
+         margin: auto;
+         transition: width 500ms;
+     }*/
 </style>
