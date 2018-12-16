7214f62b50ad2c651073d42f886e5dd996542000
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2018-12-16 09:04:15 -0500

    Quote componet, eventBus and deleteEvent

--- exercise_08/src/App.vue
+++ exercise_08/src/App.vue
@@ -5,17 +5,39 @@
         <progress-bar></progress-bar>
         <hr>
         <quote-form></quote-form>
+        <hr>
+        <section class="row">
+            <quote v-for="quote in quotes" :quote="quote"></quote>
+        </section>
     </div>
 </template>
 
 <script>
-    import ProgressBar from './components/ProgressBar.vue'
-    import QuoteForm from './components/QuoteForm.vue'
+    import ProgressBar from './components/ProgressBar.vue';
+    import QuoteForm from './components/QuoteForm.vue';
+    import Quote from './components/Quote.vue';
+    import {eventBus} from "./main";
 
     export default {
+        data() {
+            return {
+                quotes: [{id: 1, content: 'My first quoute'}]
+            }
+        },
         components: {
             progressBar: ProgressBar,
             quoteForm: QuoteForm,
+            quote: Quote
+        },
+        created() {
+            eventBus.$on('deleteQuote', (quote) => {
+                console.log('Delete event');
+                for (let index = 0; index < this.quotes.length; ++index) {
+                    if (this.quotes[index].id === quote.id) {
+                        this.quotes.splice(index, 1);
+                    }
+                }
+            })
         }
 
     }
---
+++ exercise_08/src/components/Quote.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="col-md-2" style="cursor: pointer" @click="deleteQuote(quote)">
+        {{quote.content}}
+    </div>
+</template>
+
+<script>
+    import {eventBus} from "../main";
+
+    export default {
+        props: {
+            quote: {
+                type: Object,
+                required: true
+            }
+        },
+        methods:{
+            deleteQuote(quote){
+                eventBus.$emit('deleteQuote', quote);
+            }
+        },
+        name: "Quote"
+    }
+</script>
+
+<style scoped>
+
+</style>
