b6c0e33e072c03301e2c3377dd57505e06092bfc
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2018-12-16 12:22:49 -0500

    Part 1

---
+++ exercise_10_forms/src/App.vue
@@ -0,0 +1,112 @@
+<template>
+    <div class="container">
+        <form>
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <!-- Exercise 1 -->
+                    <!-- Create a Signup Form where you retrieve the following Information -->
+                    <!-- Full Name (First Name + Last Name) -->
+                    <!-- Mail -->
+                    <!-- Password -->
+                    <!-- Store Data? Yes/No
+                    v-model="userData.fullName"
+                    v-model="userData.email"
+                     v-model="userData.password"
+                     v-model="userData.storeData"
+                      v-model="userData.storeData"
+                    -->
+                    <div class="form-group">
+                        <label for="full-name">Full Name</label>
+                        <input
+                                type="text"
+                                id="full-name"
+                                class="form-control"
+                                v-model="userData.fullName"
+                        >
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Mail</label>
+                        <input
+                                type="text"
+                                id="email"
+                                class="form-control"
+                                v-model="userData.email"
+                        >
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input
+                                type="password"
+                                id="password"
+                                class="form-control"
+                                v-model="userData.password"
+                        >
+                    </div>
+                    <div class="row">
+                        <h3>Store Data?</h3>
+                        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+                            <label for="yes">
+                                <input
+                                        type="radio"
+                                        id="yes"
+                                        value="Yes"
+                                        v-model="userData.storeData"
+                                > Yes
+                            </label>
+                            <label for="no">
+                                <input
+                                        type="radio"
+                                        id="no"
+                                        value="No"
+                                        v-model="userData.storeData"
+                                > No
+                            </label>
+                        </div>
+                    </div>
+
+                    <!-- Exercise 2 -->
+                    <!-- Only display the Form if it has NOT been submitted -->
+                    <!-- Display the Data Summary ONCE the Form HAS been submitted -->
+
+                    <!-- Exercise 3 -->
+                    <!-- Edit the Example from above and create a custom "Full Name" Control -->
+                    <!-- which still holds the First Name and Last Name Input Field -->
+                </div>
+            </div>
+        </form>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4>Your Data</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Full Name: {{userData.fullName}}</p>
+                        <p>Mail: {{userData.email}}</p>
+                        <p>Password: {{userData.password}} </p>
+                        <p>Store in Database?: {{userData.storeData}}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                userData: {
+                    fullName: '',
+                    email: '',
+                    password: '',
+                    storeData: 'No'
+                }
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
