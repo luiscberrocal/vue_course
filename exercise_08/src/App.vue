e29720e89e790e13bb43b1ccb4b97362e0a39ad2
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2018-12-16 08:08:25 -0500

    Added quote form

--- exercise_08/src/App.vue
+++ exercise_08/src/App.vue
@@ -3,15 +3,19 @@
     <div class="container">
         <h1>Quotes</h1>
         <progress-bar></progress-bar>
+        <hr>
+        <quote-form></quote-form>
     </div>
 </template>
 
 <script>
     import ProgressBar from './components/ProgressBar.vue'
+    import QuoteForm from './components/QuoteForm.vue'
 
     export default {
         components: {
-            progressBar: ProgressBar
+            progressBar: ProgressBar,
+            quoteForm: QuoteForm,
         }
 
     }
---
+++ exercise_08/src/components/QuoteForm.vue
@@ -0,0 +1,18 @@
+<template>
+    <section class="row">
+        <div class="row">
+            <textarea name="newQuout" id="" cols="100" rows="5"></textarea>
+        </div>
+        <button class="btn btn-primary">Add</button>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: "QuoteForm"
+    }
+</script>
+
+<style scoped>
+
+</style>
