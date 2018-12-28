7e6d1f18bc7da322b4cd6ca518eaa6f371d9569a
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2018-12-27 20:16:30 -0500

    Header component

--- trading_app/src/components/Home.vue
+++ trading_app/src/components/Home.vue
@@ -1,11 +1,16 @@
 <template>
+  <div>
     <h1>Home Component</h1>
+  </div>
+
 </template>
 
 <script>
-    export default {
-        name: "Home"
-    }
+
+  export default {
+    name: "Home",
+
+  }
 </script>
 
 <style scoped>
