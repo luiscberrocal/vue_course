ea00c5ec7a11234767acd19f29e069bcc7ba4107
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2018-12-27 19:53:48 -0500

    Basic components and routing

--- trading_app/src/router/index.js
+++ trading_app/src/router/index.js
@@ -1,15 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '../components/Home.vue'
+import Stocks from '../components/stocks/Stocks.vue'
+import Portfolio from '../components/portfolio/Portfolio.vue'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      name: 'home',
+      component: Home
+    },
+     {
+      path: '/portfolio',
+      name: 'portfolio',
+      component: Portfolio
+    },
+     {
+      path: '/stocks',
+      name: 'stocks',
+      component: Stocks
+    },
+
   ]
 })
