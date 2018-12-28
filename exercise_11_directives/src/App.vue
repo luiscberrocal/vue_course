7d8053d6650fca2637d04251184adef83f07ae01
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2018-12-27 19:14:07 -0500

    Excercise 11

---
+++ exercise_11_directives/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Directives Exercise</h1>
+                <button v-custom-on:click="clicked" class="btn btn-primary">Click me</button>
+                <!-- Exercise -->
+                <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            clicked() {
+                alert('I was clicked');
+            }
+        },
+        directives: {
+            customOn: {
+                bind(el, binding) {
+                    // el.onclick = function () {
+                    //     binding.value();
+                    // }
+                    const type = binding.arg;
+                    const fn = binding.value;
+                    el.addEventListener(type, fn);
+
+                }
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
