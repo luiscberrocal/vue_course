2ef46045a807f1c72a3d5b2c9165acba4d70f443
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2018-12-29 04:48:44 -0500

    Fixed disabled button

--- trading_app/src/components/stocks/Stock.vue
+++ trading_app/src/components/stocks/Stock.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="col-sm-6 col-md-4 col-xs-12">
     <div class="card">
-      <h5 class="card-header">NAME <small>100.00</small></h5>
+      <h5 class="card-header">{{ stock.name }}
+        <small>{{stock.price}}</small>
+      </h5>
       <div class="card-body">
         <div class="float-md-left float-xs-left">
-          <input type="number" class="form-control" placeholder="Quantity">
+          <input type="number" class="form-control" placeholder="Quantity" v-model="quantity">
         </div>
         <div class="float-md-right float-xs-right">
-          <button class="btn btn-success">Buy</button>
+          <button class="btn btn-success"
+                  @click="buyStock"
+                  :disabled="allowBuy">Buy
+          </button>
         </div>
       </div>
     </div>
@@ -16,6 +21,42 @@
 
 <script>
   export default {
+    props: {
+      stock: {
+        type: Object
+      },
+    },
+    data() {
+      return {
+        quantity: 0
+      }
+    },
+    computed: {
+      allowBuy() {
+        let isDecimal = !this.isInt(this.quantity);
+        let isNegative = this.quantity <= 0;
+        let disabled = isDecimal || isNegative;
+        // console.log(`Decimal: ${isDecimal}`);
+        // console.log(`Negative: ${isNegative}`);
+        // console.log(`Disabled: ${disabled}`);
+        return disabled;
+      },
+    },
+    methods: {
+      isInt(n) {
+        return n % 1 === 0;
+      },
+      buyStock() {
+        const order = {
+          stockId: this.stock.id,
+          stockPrice: this.stock.price,
+          quantity: this.quantity
+        };
+        console.log(order);
+        this.quantity = 0;
+
+      }
+    },
     name: "Stock"
   }
 </script>
