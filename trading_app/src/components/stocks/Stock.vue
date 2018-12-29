195825427de7afe10c16c20b315249cb610aa6c6
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2018-12-29 03:35:21 -0500

    Stocks boxes

--- trading_app/src/components/stocks/Stock.vue
+++ trading_app/src/components/stocks/Stock.vue
@@ -1,11 +1,23 @@
 <template>
-     <h1>Stocks - Stock Component</h1>
+  <div class="col-sm-6 col-md-4 col-xs-12">
+    <div class="card">
+      <h5 class="card-header">NAME <small>100.00</small></h5>
+      <div class="card-body">
+        <div class="float-md-left float-xs-left">
+          <input type="number" class="form-control" placeholder="Quantity">
+        </div>
+        <div class="float-md-right float-xs-right">
+          <button class="btn btn-success">Buy</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Stock"
-    }
+  export default {
+    name: "Stock"
+  }
 </script>
 
 <style scoped>
--- trading_app/src/components/stocks/Stocks.vue
+++ trading_app/src/components/stocks/Stocks.vue
@@ -1,11 +1,28 @@
 <template>
-  <h1>Stocks Component</h1>
+  <div class="row">
+    <app-stock v-for="stock in stocks"></app-stock>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Stocks"
-    }
+  import Stock from './Stock.vue'
+
+  export default {
+    data() {
+      return {
+        stocks: [
+          {id: 1, name: 'BMW', price: 110.00},
+          {id: 2, name: 'RHAT', price: 330.00},
+          {id: 3, name: 'AUDI', price: 33.00},
+          {id: 4, name: 'TYT', price: 540.00},
+        ]
+      }
+    },
+    components: {
+      appStock: Stock
+    },
+    name: "Stocks"
+  }
 </script>
 
 <style scoped>
