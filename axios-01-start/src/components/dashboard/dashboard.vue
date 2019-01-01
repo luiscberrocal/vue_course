72a4adcb0efdbd3503e3c42959dce69aec8a3dab
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2019-01-01 16:00:17 -0500

    get and post

--- axios-01-start/src/components/dashboard/dashboard.vue
+++ axios-01-start/src/components/dashboard/dashboard.vue
@@ -1,16 +1,44 @@
 <template>
-  <div id="dashboard">
-    <h1>That's the dashboard!</h1>
-    <p>You should only get here if you're authenticated!</p>
-  </div>
+    <div id="dashboard">
+        <h1>That's the dashboard!</h1>
+        <p>You should only get here if you're authenticated!</p>
+        <p>Your email is {{email}}</p>
+    </div>
 </template>
+<script>
+    import axios from 'axios';
 
+    export default {
+        data() {
+            return {
+                email: ''
+            }
+        },
+        created() {
+            const url = 'https://vuejs-course-fabc9.firebaseio.com/users.json'
+            axios.get(url)
+                .then(response => {
+                     console.log(response);
+                     const data = response.data;
+                     const users = []
+                     for (let key in data){
+                         const user = data[key];
+                         user.id = key;
+                         users.push(user);
+                     }
+                     console.log(users);
+                     this.email = users[0].email;
+                })
+                .catch(error => console.log(error));
+        }
+    }
+</script>
 <style scoped>
-  h1, p {
-    text-align: center;
-  }
+    h1, p {
+        text-align: center;
+    }
 
-  p {
-    color: red;
-  }
+    p {
+        color: red;
+    }
 </style>
